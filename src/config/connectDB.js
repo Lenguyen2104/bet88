3ddc9c022f5812c7fe833b2bff9f065f27912c2a
Letannguyen <letannguyen210496@gmail.com>
2024-03-13 09:52:09 +0700

    second commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
 
+// const connection = mysql.createPool({
+//     host: '103.54.153.32',
+//     user: 'wingo68',
+//     password: 'wingo68',
+//     database: 'wingo68',
+// });
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Thientan123',
-    database: 'new_schema',
+    password: '',
+    database: 'wingo68',
 });
 
 export default connection;
