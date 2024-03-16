a4cd5765e2eba5bb0c8f4b0edc11f3048a517fb4
nguyenlt <letannguyen@MacBook-Air-cua-Le-2.local>
2024-03-16 15:46:21 +0700

    fix sql on server

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -529,7 +529,7 @@ const promotion = async (req, res) => {
     });
   }
   const [user] = await connection.query(
-    'SELECT `phone`, `napdau`, `tongcuoc`,`money`,`code`,`invite`, `roses_f`, `roses_f1`, `roses_today` FROM users WHERE `token` = ? ',
+    'SELECT * FROM users WHERE token = ? ',
     [auth]
   );
 
