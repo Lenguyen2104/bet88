bbd04c3a6774bbc195bbf0414ad7abf35959a44e
nguyenlt <letannguyen@MacBook-Air-cua-Le-2.local>
2024-03-22 11:38:18 +0700

    fix date

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2/promise");
 
+// const connection = mysql.createPool({
+//   host: "103.54.153.32",
+//   user: "wingo68",
+//   password: "wingo68",
+//   database: "wingo68",
+// });
 const connection = mysql.createPool({
-  host: "103.54.153.32",
-  user: "wingo68",
-  password: "wingo68",
+  host: "localhost",
+  user: "root",
+  password: "",
   database: "wingo68",
 });
-// const connection = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "kiki79",
-// });
 export default connection;
