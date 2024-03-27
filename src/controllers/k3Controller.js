723751407cbb06eaf977f02ec013912f85667071
Letannguyen <letannguyen210496@gmail.com>
2024-03-28 02:06:01 +0700

    move li xi

--- src/controllers/k3Controller.js
+++ src/controllers/k3Controller.js
@@ -396,10 +396,8 @@ const addK3 = async (game) => {
     let [k5D] = await connection.query(
       `SELECT period FROM k3 WHERE status = 0 AND game = ${game} ORDER BY id DESC LIMIT 1 `
     );
-    console.log([k5D], "dasdasdasdasdasd");
     const [setting] = await connection.query("SELECT * FROM `admin` ");
     let period = k5D[0].period;
-    console.log(k5D[0].period, "perioddasdasd");
 
     let nextResult = "";
     if (game == 1) nextResult = setting[0].k3d;
